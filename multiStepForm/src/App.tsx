4f511e640bd74635a1d8e78f16847fd2d11a0798
n.jalilian <n.jalilian>
2023-02-11 17:34:32 +0330

    feature: add mui to project

--- multiStepForm/src/App.tsx
+++ multiStepForm/src/App.tsx
@@ -1,8 +1,8 @@
+import { Button } from "@mui/material";
 import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  Vite + React;
   return (
     <div className="App">
       <h1>Vite + React</h1>
